2fa173b7ebe5476b1ae0326bf51ce397a2f77011
weidi0629 <weidi.zhang0629@gmail.com>
2018-09-07 16:01:17 -0500

    Update QuadraticDiscriminantAnalysis.py

--- ch2/QuadraticDiscriminantAnalysis.py
+++ ch2/QuadraticDiscriminantAnalysis.py
@@ -4,3 +4,12 @@ X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
 y = np.array([1, 1, 1, 2, 2, 2])
 clf = QuadraticDiscriminantAnalysis()
 clf.fit(X, y)
+
+
+
+import numpy as np
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+y = np.array([1, 1, 1, 2, 2, 2])
+clf = LinearDiscriminantAnalysis()
+clf.fit(X, y)
